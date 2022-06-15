db8f21234035993b0e093489e9348593a3f70326
Assurance Ikogwe <ikogweassurance85@gmail.com>
2022-06-15 09:08:46 +0100

    Added changes to the front end of the blogger project

---
+++ Client/scripts/scripts.js
@@ -0,0 +1,19 @@
+const menuDrop = () => {
+  const menu = document.getElementById("menu");
+  const xmark = document.getElementById("xmark");
+  const bars = document.getElementById("bars");
+  
+  menu.style.display = "flex";
+  xmark.style.display = "block";
+  bars.style.display = "none";
+};
+
+const menuClose = () => {
+  const menu = document.getElementById("menu");
+  const bars = document.getElementById("bars");
+  const xmark = document.getElementById("xmark");
+  
+  menu.style.display = "none";
+  xmark.style.display = "none"
+  bars.style.display = "block";
+}
