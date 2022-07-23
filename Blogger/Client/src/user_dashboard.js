9c06b4acb7cf5e62e34d3bba88451969f711710c
Aik <ikogweassurance85@gmail.com>
2022-07-23 04:00:16 +0100

    working on the edit name and about, finished the blogger scheduler

--- Blogger/Client/src/user_dashboard.js
+++ Blogger/Client/src/user_dashboard.js
@@ -1,9 +1,8 @@
-// comments = document.querySelectorAll(".comment")
-// console.log(comments.length)
-
 //userdashboard navigation
 const nav = document.querySelectorAll(".nav");
 const content = document.querySelectorAll(".content");
+const editProfile = document.getElementById("editp");
+const home = document.getElementById("Home");
 const collapse = document.getElementById("collapse");
 const show = document.getElementById("show");
 for (let i = 0; i < nav.length; i++) {
@@ -25,52 +24,6 @@ for (let i = 0; i < nav.length; i++) {
                     content[j].classList.remove("content-active");
                 }
             }
-        }
-    });
-}
-
-const heading = document.getElementsByClassName('headings');
-const followersPost = document.getElementById('followers_post');
-const recommendedPost = document.getElementById('recommended_post');
-for (let i = 0; i < heading.length; i++) {
-    heading[i].addEventListener("click", () => {
-        if (i === 0) {
-            heading[1].classList.remove("heading-active");
-            heading[1].classList.add("heading-unactive");
-            heading[i].classList.add("heading-active");
-            followersPost.style.display = "block";
-            recommendedPost.style.display = "none";
-        }
-        if (i === 1) {
-            heading[0].classList.remove("heading-active");
-            heading[0].classList.add("heading-unactive");
-            heading[i].classList.remove("heading-unactive")
-            heading[i].classList.add("heading-active");
-            followersPost.style.display = "none";
-            recommendedPost.style.display = "block";
-        }
-    });
-}
-
-const storyHeading = document.getElementsByClassName('story_headings');
-const storiesPost = document.getElementById('stories');
-const bookmarkPost = document.getElementById('Bookmarks');
-for (let i = 0; i < storyHeading.length; i++) {
-    storyHeading[i].addEventListener("click", () => {
-        if (i === 0) {
-            storyHeading[1].classList.remove("story_heading-active");
-            storyHeading[1].classList.add("story_heading-unactive");
-            storyHeading[i].classList.add("story_heading-active");
-            storiesPost.style.display = "block";
-            bookmarkPost.style.display = "none";
-        }
-        if (i === 1) {
-            storyHeading[0].classList.remove("story_heading-active");
-            storyHeading[0].classList.add("story_heading-unactive");
-            storyHeading[i].classList.remove("story_heading-unactive")
-            storyHeading[i].classList.add("story_heading-active");
-            storiesPost.style.display = "none";
-            bookmarkPost.style.display = "block";
         }
     });
 }
@@ -106,7 +59,29 @@ show.addEventListener("click", () => {
     }
 });
 
-//variables for uploading image 
+//userdashboard edit profile; changing username
+const userImage = document.getElementById("user_image");
+const userName = document.getElementById("user_name");
+const user = document.getElementById("name");
+const about = document.getElementById("about");
+user.placeholder = userName.innerHTML;
+about.placeholder = "No about yet";
+
+user.nextElementSibling.addEventListener("click", () => {
+    user.parentElement.classList.toggle("edit_info-active");
+});
+
+about.nextElementSibling.addEventListener("click", () => {
+    about.parentElement.classList.toggle("edit_info-active");
+});
+
+//link to profile by clicking on user image
+userImage.addEventListener("click", ()=>{
+    editProfile.classList.toggle("editp-active");
+    home.classList.remove("content-active");
+})
+
+//Uploading image 
 const uploadImage = document.getElementById("upload_image");
 const edit = document.querySelectorAll(".edit");
 const camera = document.getElementById("camera");
@@ -118,7 +93,6 @@ camera.addEventListener("click", () => {
     camera.style.display = "none";
     camera.previousElementSibling.style.display = "none";
 });
-
 
 //upload image with FileReader
 uploadImage.addEventListener("change", () => {
@@ -153,8 +127,55 @@ for (let i = 0; i < Options.length; i++) {
     });
 }
 
-// var numberOfLikes = []
+//userdashboard home section, following and recommended headings
+const heading = document.getElementsByClassName('headings');
+const followersPost = document.getElementById('followers_post');
+const recommendedPost = document.getElementById('recommended_post');
+for (let i = 0; i < heading.length; i++) {
+    heading[i].addEventListener("click", () => {
+        if (i === 0) {
+            heading[1].classList.remove("heading-active");
+            heading[1].classList.add("heading-unactive");
+            heading[i].classList.add("heading-active");
+            followersPost.style.display = "block";
+            recommendedPost.style.display = "none";
+        }
+        if (i === 1) {
+            heading[0].classList.remove("heading-active");
+            heading[0].classList.add("heading-unactive");
+            heading[i].classList.remove("heading-unactive")
+            heading[i].classList.add("heading-active");
+            followersPost.style.display = "none";
+            recommendedPost.style.display = "block";
+        }
+    });
+}
 
+//userdashboard story section, stories and bookmark headings
+const storyHeading = document.getElementsByClassName('story_headings');
+const storiesPost = document.getElementById('stories');
+const bookmarkPost = document.getElementById('Bookmarks');
+for (let i = 0; i < storyHeading.length; i++) {
+    storyHeading[i].addEventListener("click", () => {
+        if (i === 0) {
+            storyHeading[1].classList.remove("story_heading-active");
+            storyHeading[1].classList.add("story_heading-unactive");
+            storyHeading[i].classList.add("story_heading-active");
+            storiesPost.style.display = "block";
+            bookmarkPost.style.display = "none";
+        }
+        if (i === 1) {
+            storyHeading[0].classList.remove("story_heading-active");
+            storyHeading[0].classList.add("story_heading-unactive");
+            storyHeading[i].classList.remove("story_heading-unactive")
+            storyHeading[i].classList.add("story_heading-active");
+            storiesPost.style.display = "none";
+            bookmarkPost.style.display = "block";
+        }
+    });
+}
+
+//userdashboard user's engagements on post; likes, comments, bookmark
 const addComment = document.querySelectorAll(".add_comment");
 const sendComment = document.querySelectorAll(".send_comment");
 const actualComment = document.querySelectorAll(".comment")
@@ -164,12 +185,12 @@ const commentInput = document.querySelectorAll(".comment_input");
 const thumbs = document.querySelectorAll("#thumbs");
 const markBook = document.getElementById("markbook");
 const likes = document.querySelectorAll("#like");
-const userImage = document.getElementById("user_image");
-const userName = document.getElementById("user_name");
 for (let i = 0; i < regular.length; i++) {
     regular[i].addEventListener("click", () => {
         regular[i].classList.toggle("fa-solid");
         for (let j = 0; j < thumbs.length; j++) {
+
+            //code for likes
             if (regular[i] == thumbs[j]) {
                 let like = Number(regular[i].firstElementChild.innerHTML);
                 like++;
@@ -181,14 +202,19 @@ for (let i = 0; i < regular.length; i++) {
                     likes[j].firstElementChild.innerHTML = "Like";
                 })
             }
+
+            //code for bookmark
             if (regular[i] == markBook) {
                 const followersPost = document.getElementById('followers_post_card');
                 const bookmark = document.getElementById("Bookmarks");
                 const clonedNode = followersPost.cloneNode(true);
                 return bookmark.appendChild(clonedNode);
             }
+
+            //code for comments
             if (regular[i] == comments[j]){
 
+                //adding comments
                 addComment[j].classList.toggle("add_comment-active");
                 for(let k = 0; k < addComment.length; k++){
                     if(addComment[k] !== addComment[j] && addComment[k].classList.contains("add_comment-active")){
@@ -197,8 +223,10 @@ for (let i = 0; i < regular.length; i++) {
                     }
                 }
                 
+                //sending comments
                 sendComment[j].addEventListener("click", () => {
                     const date = new Date();
+                    const enUsDateFormatter = new Intl.DateTimeFormat('en-Us');
                     if(commentInput[j].firstElementChild.value){
                         userComment = actualComment[j].cloneNode(true)
                         userComment.classList.add("cloned")
@@ -207,16 +235,19 @@ for (let i = 0; i < regular.length; i++) {
                         userComment.firstElementChild.firstElementChild.src = userImage.src;
                         userComment.firstElementChild.firstElementChild.nextElementSibling.innerHTML = userName.innerHTML;
                         userComment.firstElementChild.nextElementSibling.innerHTML = commentInput[j].firstElementChild.value;
-                        userComment.firstElementChild.nextElementSibling.nextElementSibling.firstElementChild.innerHTML = date.toDateString();
+                        userComment.firstElementChild.nextElementSibling.nextElementSibling.firstElementChild.innerHTML = enUsDateFormatter.format(date);
                         commentInput[j].firstElementChild.value = "";
                         let deleteComment = document.querySelectorAll(".delete_cloned");    
                         let cloned = document.querySelectorAll(".cloned");
+
+                        //deleting comments
                         for(let k = 0; k < deleteComment.length; k++){ 
                             deleteComment[k].addEventListener("click", () =>{
                             cloned[k].remove();
                             });
-                        }             
-                    } else {
+                        }  
+
+                    } else { //a little animation for when the inpt field is empty
                         var pos = 20;
                         const move = () => {
                             pos= pos + 5
@@ -235,6 +266,7 @@ for (let i = 0; i < regular.length; i++) {
     });
 }
 
+//code for adding replies
 const addReply = document.querySelectorAll(".add_reply");
 const commentReaction = document.querySelectorAll(".comment_reaction");
 const replies = document.querySelectorAll(".replies");
@@ -260,14 +292,15 @@ for(let i = 0; i < replies.length; i++){
     });
 }
 
+//userdashboard scheduler for scheduling posts
 const fixSchedule = document.getElementById("schedule");
-
 schedule.addEventListener("click", () => {
   alert("Welcome, please set the date and time you wish to publish your post");
 });
 
+//custom function to replace the native alert box function
 window.alert = function(alert_message){
-    customAlert(alert_message) //custom function to replace the native alert box function
+    customAlert(alert_message) 
   } 
 
 //the custom function  
@@ -343,6 +376,9 @@ function customAlert(alert_message){
         body.removeChild(alertContainer);
     })
 }
+
+
+
 
 
 
