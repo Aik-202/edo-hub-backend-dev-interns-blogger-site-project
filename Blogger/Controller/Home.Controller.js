e8473fe26db23448064f4bc766ec02a5c981e307
Aik <ikogweassurance85@gmail.com>
2022-07-26 13:09:28 +0100

    I changed the Home.html to index.html in the controller folder

--- Blogger/Controller/Home.Controller.js
+++ Blogger/Controller/Home.Controller.js
@@ -1,7 +1,7 @@
 const path=require('path');
 
 function HomeGetController(req,res){
-    res.sendFile(path.join(__dirname, '..', 'Client', 'public', 'Home.html',));
+    res.sendFile(path.join(__dirname, '..', 'Client', 'public', 'index.html',));
 }
 module.exports={
     HomeGetController,
